c8f7c87b9d01ca170436366cf0c7fb89929bd3e7
Amine <amine.touba59@gmail.com>
2023-06-22 12:45:46 +0200

    add basename browserrouter

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <React.StrictMode>
       <div className='body'>
         <Nav />
